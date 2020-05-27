c4efd2ba2e357d9d03a6614cb120c4e3a19750f8
Andrew Nelson <andyfaff@gmail.com>
2020-05-27 12:23:02 +1000

    MAINT: all refl1d backends

--- validation/scripts/refl1d_runner.py
+++ validation/scripts/refl1d_runner.py
@@ -1,20 +1,27 @@
+import itertools
 import numpy as np
 from test_discovery import get_test_data
 
 from refl1d import abeles
+from refl1d.reflectivity import reflectivity_amplitude
 
 
 def run_tests():
+    # abeles.refl is a Python calculator, reflectivity_amplitude uses
+    # a C extension
+    f_amplitudes = [abeles.refl, reflectivity_amplitude]
 
-    for slabs, data in get_test_data():
-        z = abeles.refl(
+    for f_amplitude, test in itertools.product(f_amplitudes, get_test_data()):
+        slabs, data = test
+
+        r = f_amplitude(
             data[:, 0] / 2.0,
             slabs[:, 0],
             slabs[:, 1],
             irho=slabs[:, 2],
             sigma=slabs[1:, 3],
         )
-        R = z.real ** 2 + z.imag ** 2
+        R = (r * np.conj(r)).real
 
         assert R.shape == data[:, 1].shape
 
