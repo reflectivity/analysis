6246285f482b11a051e611742ca0039df6701246
Andrew Nelson <andyfaff@gmail.com>
2020-05-27 12:12:34 +1000

    MAINT: check all refnx backends

--- validation/scripts/refnx_runner.py
+++ validation/scripts/refnx_runner.py
@@ -1,13 +1,18 @@
+import itertools
 import numpy as np
 from test_discovery import get_test_data
 
-from refnx.reflect.reflect_model import abeles
+from refnx.reflect.reflect_model import abeles, use_reflect_backend
 
 
 def run_tests():
+    # cython backend may or may not be present on all systems
+    backends = ["c", "python", "cython"]
 
-    for slabs, data in get_test_data():
-        R = abeles(data[:, 0], slabs)
+    for backend, test in itertools.product(backends, get_test_data()):
+        slabs, data = test
+        with use_reflect_backend(backend) as abeles:
+            R = abeles(data[:, 0], slabs)
         assert R.shape == data[:, 1].shape
 
         np.testing.assert_allclose(R, data[:, 1], rtol=8e-5)
