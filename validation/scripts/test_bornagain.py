e7b34fc719e90b4b1be9a82d8ca8932daf677b93
Andrew Nelson <andyfaff@gmail.com>
2023-02-06 15:54:08 +1100

    MAINT: ba.RangedDistributionGaussian doesn't exist anymore

--- validation/scripts/test_bornagain.py
+++ validation/scripts/test_bornagain.py
@@ -57,8 +57,9 @@ def get_simulation_smeared(qzs, dqzs, sample):
     # 3.5 sigma to sync with refnx
     n_sig = 3.5
     n_samples = 21
-    distr = ba.RangedDistributionGaussian(n_samples, n_sig)
 
+    distr = ba.DistributionGaussian(0., 1., 21, 3.5)
+    scan.setVectorResolution(distr, dqzs * 10.0)
     scan = ba.QzScan(qzs * 10.0)
     scan.setAbsoluteQVectorResolution(distr, dqzs * 10.0)
 
